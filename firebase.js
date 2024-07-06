9d64507f19f1c39102e6fc402b9f4de58fcb6dee
Harshaayyappan <133531679+harshaayyappan@users.noreply.github.com>
2024-07-06 14:05:28 +0530

    Add files via upload

---
+++ firebase.js
@@ -0,0 +1,17 @@
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+import {initializeApp} from 'firebase/app'
+
+
+
+
+const firebaseConfig = {
+    apiKey: "AIzaSyAUWdYGVeiQLhPI3L6pFCoGyq4mxZSFLpw",
+    authDomain: "expense-tracker-1e649.firebaseapp.com",
+    projectId: "expense-tracker-1e649",
+    storageBucket: "expense-tracker-1e649.appspot.com",
+    messagingSenderId: "630224303147",
+    appId: "1:630224303147:web:82e08f540229164d9b666e",
+    measurementId: "G-VT4PDZWLSG"
+  };
+
+  initializeApp(firebaseConfig)
